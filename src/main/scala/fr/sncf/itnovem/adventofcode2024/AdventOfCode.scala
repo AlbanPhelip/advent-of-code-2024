0499d4bb7d4423052aa1fca4bd27559e6ac8edde
aphelip <alban.phelip@gmail.com>
2024-11-26 13:07:19 +0100

    Initial commit

---
+++ src/main/scala/fr/sncf/itnovem/adventofcode2024/AdventOfCode.scala
@@ -0,0 +1,26 @@
+package fr.sncf.itnovem.adventofcode2024
+
+import scala.io.Source
+
+trait AdventOfCode {
+
+  def fileName: String
+
+  def execute(): (Any, Any)
+
+  val input: Array[String] = {
+    val file = Source.fromFile(getClass.getResource(s"../../../../$fileName").getPath)
+    val input = file.getLines.toArray
+    file.close()
+    input
+  }
+
+  def main(args: Array[String]): Unit = {
+    val (star1, star2) = execute()
+    println("****************")
+    println(s"Result for star 1 is $star1")
+    println(s"Result for star 2 is $star2")
+    println("****************")
+    println()
+  }
+}
