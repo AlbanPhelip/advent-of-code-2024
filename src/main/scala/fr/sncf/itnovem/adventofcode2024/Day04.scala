d8cdb48915b967569efbeee8b91c26310c0abfb5
aphelip <alban.phelip@gmail.com>
2024-12-04 10:02:49 +0100

    Add Day 4

---
+++ src/main/scala/fr/sncf/itnovem/adventofcode2024/Day04.scala
@@ -0,0 +1,42 @@
+package fr.sncf.itnovem.adventofcode2024
+
+import scala.util.{Success, Try}
+
+object Day04 extends AdventOfCode {
+
+  override def fileName: String = "aoc04.txt"
+
+  override def execute(): (Long, Long) = {
+    val data: Array[Array[String]] = input.map(_.split(""))
+
+    val star1 = data.indices.map { y =>
+      data(y).indices.map { x =>
+        if (data(y)(x) == "X") {
+          Array(
+            Try(data(y - 1)(x) + data(y - 2)(x) + data(y - 3)(x)),
+            Try(data(y + 1)(x) + data(y + 2)(x) + data(y + 3)(x)),
+            Try(data(y)(x - 1) + data(y)(x - 2) + data(y)(x - 3)),
+            Try(data(y)(x + 1) + data(y)(x + 2) + data(y)(x + 3)),
+            Try(data(y + 1)(x + 1) + data(y + 2)(x + 2) + data(y + 3)(x + 3)),
+            Try(data(y - 1)(x + 1) + data(y - 2)(x + 2) + data(y - 3)(x + 3)),
+            Try(data(y - 1)(x - 1) + data(y - 2)(x - 2) + data(y - 3)(x - 3)),
+            Try(data(y + 1)(x - 1) + data(y + 2)(x - 2) + data(y + 3)(x - 3))
+          ).count(_ == Success("MAS"))
+        } else 0
+      }.sum
+    }.sum
+
+    val star2 = data.indices.map { y =>
+      data(y).indices.count { x =>
+        (data(y)(x) == "A") && {
+          val diagonalNorthEast = Array(Try(data(y - 1)(x + 1)).getOrElse(""), Try(data(y + 1)(x - 1)).getOrElse("")).sorted.mkString("")
+          val diagonalSouthEast = Array(Try(data(y - 1)(x - 1)).getOrElse(""), Try(data(y + 1)(x + 1)).getOrElse("")).sorted.mkString("")
+
+          diagonalNorthEast == "MS" && diagonalSouthEast == "MS"
+        }
+      }
+    }.sum
+
+    (star1, star2)
+  }
+}
